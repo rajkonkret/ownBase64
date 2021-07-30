874a459e80da6ecefcec824dc14da1361dc83823
rajkonkret660@gmail.com <radoslaw.janiak@softpos.eu>
2021-07-30 14:34:56 +0200

    add decoder from SOvFl

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,13 @@ import java.util.Base64;
 public class Main {
     public static void main(String[] args) {
 
+
         String input = "ABC";
         System.out.println(Base64.getEncoder().encodeToString(input.getBytes()));
         String ownBase64 = ownBase64Encode(input);
         System.out.println(ownBase64);
         System.out.println(ownBase64.length());
+        System.out.println(new String(decode("QUJD")));
     }
 
     static public String ownBase64Encode(String s) {
@@ -44,5 +46,43 @@ public class Main {
         }
 
         return output;
+    }
+
+    /**
+     * Translates the specified Base64 string into a byte array.
+     *
+     * @param s the Base64 string (not null)
+     * @return the byte array (not null)
+     */
+    public static byte[] decode(String s){
+        final char[] ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".toCharArray();
+        int[]  toInt   = new int[128];
+
+            for(int i=0; i< ALPHABET.length; i++){
+                toInt[ALPHABET[i]]= i;
+            }
+
+
+
+        int delta = s.endsWith( "==" ) ? 2 : s.endsWith( "=" ) ? 1 : 0;
+        byte[] buffer = new byte[s.length()*3/4 - delta];
+        int mask = 0xFF;
+        int index = 0;
+        for(int i=0; i< s.length(); i+=4){
+            int c0 = toInt[s.charAt( i )];
+            int c1 = toInt[s.charAt( i + 1)];
+            buffer[index++]= (byte)(((c0 << 2) | (c1 >> 4)) & mask);
+            if(index >= buffer.length){
+                return buffer;
+            }
+            int c2 = toInt[s.charAt( i + 2)];
+            buffer[index++]= (byte)(((c1 << 4) | (c2 >> 2)) & mask);
+            if(index >= buffer.length){
+                return buffer;
+            }
+            int c3 = toInt[s.charAt( i + 3 )];
+            buffer[index++]= (byte)(((c2 << 6) | c3) & mask);
+        }
+        return buffer;
     }
 }
